33f81185c7de18b0365a1308023a82d4a545d375
Victor Facundo <victorfacundo122@hotmail.com>
2024-03-20 19:28:36 -0300

    implementação find all

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,6 +27,14 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n === Test 3 : Seller find ALL === ");
+		
+		 list = sellerDao.findAll();
+		 for(Seller obj : list) {
+			 System.out.println(obj);
+		 }
+		
 
 		
 		
