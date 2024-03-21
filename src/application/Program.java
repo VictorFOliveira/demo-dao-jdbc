4ddf14ca93371bdea012435c96289447c7110143
Victor Facundo <victorfacundo122@hotmail.com>
2024-03-21 10:54:42 -0300

    Delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -13,7 +14,7 @@ public class Program {
 	public static void main(String[] args) {
 
 		
-		
+		Scanner sc  = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println("=== Test 1 : Seller find by ID === ");
 		Seller seller = sellerDao.findById(3);
@@ -56,6 +57,17 @@ public class Program {
 			sellerDao.update(seller);
 			
 			System.out.println("Update Complete");
+			
+			
+			System.out.println("\n === Test 6 : Seller DELETE === ");
+			
+			System.out.println("Enter id for delete test");
+			
+			int id = sc.nextInt();
+			
+			sellerDao.deleteById(id);
+			System.out.println("Delete complete");
+			sc.close();
 
 		
 
